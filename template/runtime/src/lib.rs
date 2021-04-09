35404e1a9cb5cbcf16d1058fcbc44042a87140bd
canguruhh <sven@mutzl.org>
2021-04-09 13:18:56 +0000

    rename node

--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -100,8 +100,8 @@ pub mod opaque {
 }
 
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-frontier-template"),
-	impl_name: create_runtime_str!("node-frontier-template"),
+	spec_name: create_runtime_str!("metaverse-vanilla"),
+	impl_name: create_runtime_str!("metaverse-vanilla"),
 	authoring_version: 1,
 	spec_version: 1,
 	impl_version: 1,
