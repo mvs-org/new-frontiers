57fc02a9f1a17c9a1b0a6563d510b765f5f3d193
sylvaincormier <sylvaincormier@protonmail.com>
2021-05-06 14:27:28 -0400

    Accounts from seed in chain_spec

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -153,6 +153,7 @@ parameter_types! {
 	pub BlockLength: frame_system::limits::BlockLength = frame_system::limits::BlockLength
 		::max_with_normal_ratio(5 * 1024 * 1024, NORMAL_DISPATCH_RATIO);
 	pub const SS58Prefix: u8 = 150;
+	
 }
 
 // Configure FRAME pallets to include in runtime.
