8bd73e4ed4c92529fb22e2da58fd35183156a904
sylvaincormier <sylvaincormier@protonmail.com>
2021-05-21 08:27:27 -0400

    Revert "hybrid consensus not producing blocks"
    This reverts commit 3d737ed4d49d41cbdb5ae7a83a5954f1f9603268.

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,11 +1,9 @@
 use sp_core::{Pair, Public, sr25519};
 use metaverse_vm_runtime::{
-	AccountId, BalancesConfig, GenesisConfig, GrandpaConfig,
+	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
 	SudoConfig, SystemConfig, EVMConfig, EthereumConfig, WASM_BINARY, Signature
 };
-
-// use sp_core::sr25519;
-
+use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{Verify, IdentifyAccount};
 use sc_service::{ChainType, Properties};
@@ -34,9 +32,12 @@ pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
 	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
 
-
-pub fn authority_keys_from_seed(seed: &str) -> GrandpaId {
-	get_from_seed::<GrandpaId>(seed)
+/// Generate an Aura authority key.
+pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
+	(
+		get_from_seed::<AuraId>(s),
+		get_from_seed::<GrandpaId>(s),
+	)
 }
 
 pub fn properties() -> Properties {
@@ -137,13 +138,10 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 	))
 }
 
-
-
-
 /// Configure initial storage state for FRAME modules.
 fn testnet_genesis(
 	wasm_binary: &[u8],
-	initial_authorities: Vec<GrandpaId>,
+	initial_authorities: Vec<(AuraId, GrandpaId)>,
 	root_key: AccountId,
 	endowed_accounts: Vec<AccountId>,
 	_enable_println: bool,
@@ -196,8 +194,11 @@ fn testnet_genesis(
 			// Configure endowed accounts with initial balance of 1 << 60.
 			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
 		}),
+		pallet_aura: Some(AuraConfig {
+			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
+		}),
 		pallet_grandpa: Some(GrandpaConfig {
-			authorities: initial_authorities.iter().map(|x| (x.clone(), 1)).collect(),
+			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
 		}),
 		pallet_sudo: Some(SudoConfig {
 			// Assign network admin rights.
