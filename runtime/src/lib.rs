64139195dd02efdd714f03bfb59f12057f8bd41f
tristan <tristan.lee@qq.com>
2021-11-30 21:26:58 +0800

    set gas price to 10Gwei

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -124,7 +124,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 100,
+	spec_version: 101,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
@@ -335,8 +335,8 @@ pub struct FixedGasPrice;
 
 impl FeeCalculator for FixedGasPrice {
 	fn min_gas_price() -> U256 {
-		// Gas price is always one token per gas.
-		1.into()
+		// Gas price is 10 gwei
+		10_000_000_000u64.into()
 	}
 }
 
